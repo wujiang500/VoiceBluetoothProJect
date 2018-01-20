8119d88c68c3a8ad4f012ccab6ccda77ac05ebf8
吴疆 <wujiang_500@pku.edu.cn>
2018-01-20 17:00:44 +0800

    Initial Commit

---
+++ TeleController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TeleController
+//
+//  Created by wujiang on 2018/1/20.
+//  Copyright © 2018年 wujiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TeleController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TeleController
+//
+//  Created by wujiang on 2018/1/20.
+//  Copyright © 2018年 wujiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
